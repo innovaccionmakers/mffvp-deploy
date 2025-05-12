356886d970baf5f979e3fbe2168c79fb32117fa6
MAKERS-INNOVACCION <206225552+MAKERS-INNOVACCION@users.noreply.github.com>
2025-05-12 06:38:48 -0500

    Update Program.cs

--- src/API/MFFVP.Api/Program.cs
+++ src/API/MFFVP.Api/Program.cs
@@ -17,9 +17,6 @@ using Common.SharedKernel.Infrastructure.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
 builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
