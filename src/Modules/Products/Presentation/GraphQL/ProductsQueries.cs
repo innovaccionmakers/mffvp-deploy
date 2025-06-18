7f5b1f6ddabb8d5811254d055fa68d3d7e216fac
Eder Andres Chavez Ruiz <ederacruiz@MacBook-Pro-de-Eder.local>
2025-06-17 19:51:31 -0500

    feature: added query handler

--- src/Modules/Products/Presentation/GraphQL/ProductsQueries.cs
+++ src/Modules/Products/Presentation/GraphQL/ProductsQueries.cs
@@ -12,21 +12,35 @@ public class ProductsQueries
         [Service] IMediator mediator, 
         CancellationToken cancellationToken)
     {
-        var result = await mediator.Send(new GetPortfolioInformationByObjetiveIdQuery(objetiveId), cancellationToken);
 
-        if (!result.IsSuccess || result.Value == null)
+        try
         {
-            throw new InvalidOperationException("Failed to retrieve portfolio information.");
+
+            var result = await mediator.Send(new GetPortfolioInformationByObjetiveIdQuery(objetiveId), cancellationToken);
+
+            if (!result.IsSuccess || result.Value == null)
+            {
+                throw new InvalidOperationException("Failed to retrieve portfolio information.");
+            }
+
+            var portfolioInformation = result.Value;
+
+
+            return new PortfolioDto(
+            portfolioInformation.Found,
+            portfolioInformation.Plan,
+            portfolioInformation.Alternative,
+            portfolioInformation.Portfolio);
+
         }
-
-        var portfolioInformation = result.Value;
-
-
-        return new PortfolioDto(
-           portfolioInformation.Found,
-           portfolioInformation.Plan,
-           portfolioInformation.Alternative,
-           portfolioInformation.Portfolio);     
+        catch (Exception ex)
+        {
+                // Puedes loguear el error aquí si tienes un sistema de logging
+            throw new GraphQLException(ErrorBuilder.New()
+            .SetMessage("Ocurrió un error inesperado al obtener el portafolio.")
+            .SetCode("UNEXPECTED_ERROR")
+            .Build());
+        }
 
     }
 }
