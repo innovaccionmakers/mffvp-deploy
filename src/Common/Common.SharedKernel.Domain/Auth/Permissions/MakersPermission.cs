c05bd44ce8777bc1b4b4de9cd3192528fb2a18ba
MAKERS-INNOVACCION <207471848+innovaccionmakers@users.noreply.github.com>
2025-07-02 15:47:17 -0500

    Update MakersPermission.cs

--- src/Common/Common.SharedKernel.Domain/Auth/Permissions/MakersPermission.cs
+++ src/Common/Common.SharedKernel.Domain/Auth/Permissions/MakersPermission.cs
@@ -3,7 +3,7 @@
 public record MakersPermission(string Description, string Module, string Domain, string Resource, string Action)
 {
     public string Name => NameFor(Module, Domain, Action, Resource);
-    public static string NameFor(string module, string domain, string action, string resource)
+    public static string NameFor(string module, string domain, string resource, string action)
     {
         return $"{module}:{domain}:{resource}:{action}";
     }
