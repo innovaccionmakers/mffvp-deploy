bb55908dea3bfc236b083b1affd27d7819e2c24a
Katty Mizhquero <innovaccion.makersdev8@outlook.com>
2025-07-07 15:51:58 -0500

    change column name for commission

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Products/20250707201228_ChangeStatusCommissions.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE productos.comisiones RENAME COLUMN activo TO estado;
+
+INSERT INTO productos."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250707201228_ChangeStatusCommissions', '9.0.3');
+
+
