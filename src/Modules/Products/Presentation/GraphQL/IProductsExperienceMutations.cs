ca9d51289c07685dd5f5421f7c6e557c5ddc3d9c
Eder Andres Chavez Ruiz <andres.chavez@somosmakers.co>
2025-07-08 17:31:18 -0500

    feat: implement RegisterGoal mutation and related input model

---
+++ src/Modules/Products/Presentation/GraphQL/IProductsExperienceMutations.cs
@@ -0,0 +1,15 @@
+﻿using Common.SharedKernel.Presentation.Results;
+using FluentValidation;
+using Products.Presentation.DTOs;
+using Products.Presentation.GraphQL.Input;
+
+namespace Products.Presentation.GraphQL;
+
+public interface IProductsExperienceMutations
+{
+    public Task<GraphqlMutationResult<GoalMutationResult>> RegisterGoalAsync(
+        CreateGoalInput input,
+        IValidator<CreateGoalInput> validator,
+        CancellationToken cancellationToken = default
+    );
+}
---
+++ src/Modules/Products/Presentation/GraphQL/ProductsExperienceMutations.cs
@@ -0,0 +1,64 @@
+﻿using Common.SharedKernel.Domain;
+using Common.SharedKernel.Presentation.Filters;
+using Common.SharedKernel.Presentation.Results;
+using FluentValidation;
+using MediatR;
+using Products.Integrations.Objectives.CreateObjective;
+using Products.Presentation.DTOs;
+using Products.Presentation.GraphQL.Input;
+
+namespace Products.Presentation.GraphQL;
+
+public class ProductsExperienceMutations(IMediator mediator) : IProductsExperienceMutations
+{
+    public async Task<GraphqlMutationResult<GoalMutationResult>> RegisterGoalAsync(CreateGoalInput input, IValidator<CreateGoalInput> validator, CancellationToken cancellationToken = default)
+    {
+        var result = new GraphqlMutationResult<GoalMutationResult>();
+        try
+        {
+
+            var validationResult = await RequestValidator.Validate(input, validator);
+
+            if (validationResult is not null)
+            {
+                result.AddError(validationResult.Error);
+                return result;
+            }
+
+            var command = new CreateObjectiveCommand(
+                input.IdType,
+                input.Identification,
+                input.AlternativeId,
+                input.ObjectiveType,
+                input.ObjectiveName,
+                input.OpeningOffice,
+                input.CurrentOffice,
+                input.Commercial
+            );
+
+            var commandResult = await mediator.Send(command, cancellationToken);
+
+            if (!commandResult.IsSuccess)
+            {
+                result.AddError(commandResult.Error);
+                return result;
+            }
+
+            var response = new GoalMutationResult(
+                commandResult.Value.ObjectiveId ?? 0,
+                "Objetivo",
+                null
+            );
+
+            result.SetSuccess(response, "Genial!, Se ha creado el objetivo del Afiliado");
+            return result;
+
+        }
+        catch (Exception ex)
+        {
+            result.AddError(new Error("EXCEPTION", ex.Message, ErrorType.Failure));
+            return result;
+        }
+
+    }
+}
