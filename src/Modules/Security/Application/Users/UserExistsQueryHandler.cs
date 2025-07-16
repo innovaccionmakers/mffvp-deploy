8e65a906e69b5fae567a7226275031d32a1f3bc4
MAKERS-INNOVACCION <207471848+innovaccionmakers@users.noreply.github.com>
2025-07-16 14:43:56 -0500

    ValidarUsuarioInsertarUsuario

---
+++ src/Modules/Security/Application/Users/UserExistsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Common.SharedKernel.Application.Messaging;
+using Common.SharedKernel.Domain;
+
+using Security.Application.Contracts.Users;
+using Security.Domain.Users;
+
+namespace Security.Application.Users;
+
+public sealed class UserExistsQueryHandler(
+    IUserRepository userRepository)
+    : IQueryHandler<UserExistsQuery, bool>
+{
+    public async Task<Result<bool>> Handle(UserExistsQuery request, CancellationToken cancellationToken)
+    {
+        var exists = await userRepository.ExistsAsync(request.UserId, cancellationToken);
+        return Result.Success(exists);
+    }
+}
