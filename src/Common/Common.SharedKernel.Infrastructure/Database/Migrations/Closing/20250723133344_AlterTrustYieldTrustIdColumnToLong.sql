6b0737ab3afea2ac2d79cdadaa2599e4f5d5aef7
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-07-23 08:36:12 -0500

    refactor: 💡 change TrustId column type to long

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Closing/20250723133344_AlterTrustYieldTrustIdColumnToLong.sql
@@ -0,0 +1,7 @@
+﻿START TRANSACTION;
+ALTER TABLE cierre.rendimientos_fideicomisos ALTER COLUMN fideicomiso_id TYPE bigint;
+
+INSERT INTO cierre."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250723133344_AlterTrustYieldTrustIdColumnToLong', '9.0.3');
+
+
