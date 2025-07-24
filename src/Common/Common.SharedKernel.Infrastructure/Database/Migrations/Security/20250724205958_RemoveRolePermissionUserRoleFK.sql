56289acaeaa9fa86655ebd5b40faa581a262e155
MAKERS-INNOVACCION <207471848+innovaccionmakers@users.noreply.github.com>
2025-07-24 16:08:35 -0500

    constraint

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Security/20250724205958_RemoveRolePermissionUserRoleFK.sql
@@ -0,0 +1,10 @@
+﻿ALTER TABLE seguridad.usuarios_roles DROP CONSTRAINT "FK_usuarios_roles_roles_permisos_RolePermissionId";
+
+DROP INDEX seguridad."IX_usuarios_roles_RolePermissionId";
+
+ALTER TABLE seguridad.usuarios_roles DROP COLUMN "RolePermissionId";
+
+INSERT INTO seguridad."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250724205958_RemoveRolePermissionUserRoleFK', '9.0.3');
+
+
