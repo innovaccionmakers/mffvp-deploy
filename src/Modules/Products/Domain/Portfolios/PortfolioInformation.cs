e22340723cde87c00d96a69b0383de41856081ee
Eder Andres Chavez Ruiz <andres.chavez@somosmakers.co>
2025-07-24 12:38:22 -0500

    feat: add new information for portfolio information, add new condition to filter just collector portfolios

--- src/Modules/Products/Domain/Portfolios/PortfolioInformation.cs
+++ src/Modules/Products/Domain/Portfolios/PortfolioInformation.cs
@@ -4,9 +4,13 @@ namespace Products.Domain.Portfolios;
 public class PortfolioInformation
 {
     public string Found { get; set; }
+    public int FoundId { get; set; }
     public string Plan { get; set; }
+    public int PlanId { get; set; }
     public string Alternative { get; set; }
+    public int AlternativeId { get; set; }
     public string Portfolio { get; set; }
+    public int PortfolioId { get; set; }
 
     public PortfolioInformation()
     {
@@ -15,16 +19,24 @@ public class PortfolioInformation
 
     public static PortfolioInformation Create(
         string found,
+        int foundId,
         string plan,
+        int planId,
         string alternative,
-        string portfolio)
+        int alternativeId,
+        string portfolio,
+        int portfolioId)
     {
         return new PortfolioInformation
         {
             Found = found,
+            FoundId = foundId,
             Plan = plan,
+            PlanId = planId,
             Alternative = alternative,
-            Portfolio = portfolio
+            AlternativeId = alternativeId,
+            Portfolio = portfolio,
+            PortfolioId = portfolioId
         };
     }
 }
