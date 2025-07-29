c028f7e518281ac2cf81b68afec8777e13d9c83e
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-07-29 14:40:41 -0500

    feat: 🎸 add Processed flag to TemporaryClientOperation

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Operations/20250729193740_AddProcessedToTemporaryClientOperations.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE operaciones.operaciones_clientes_temporal ADD procesado boolean NOT NULL DEFAULT FALSE;
+
+INSERT INTO operaciones."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250729193740_AddProcessedToTemporaryClientOperations', '9.0.3');
+
+
