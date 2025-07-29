cf30ff277520865b3d5c87a0a988720ecc2a8631
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-07-29 17:14:19 -0500

    feat: 🎸 filter subtransaction types by category

---
+++ src/Modules/Operations/Domain/SubtransactionTypes/SubtransactionTypeCategoryUuids.cs
@@ -0,0 +1,6 @@
+namespace Operations.Domain.SubtransactionTypes;
+
+public static class SubtransactionTypeCategoryUuids
+{
+    public static readonly Guid Contribution = Guid.Parse("5e0a0993-88ea-4785-b3d1-a2813fa2f3d5");
+}
