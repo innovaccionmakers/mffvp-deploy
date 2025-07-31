9ee1fe4516ce39d6bb332e2c20df6377fe271df4
Eder Andres Chavez Ruiz <andres.chavez@somosmakers.co>
2025-07-31 11:52:36 -0500

    refactor(operations): change user id from int to string in auxiliary info, add migration

---
+++ src/Modules/Operations/Infrastructure/Database/Migrations/20250731154823_ChangeUsuarioIdToString.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Operations.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUsuarioIdToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "usuario_id",
+                schema: "operaciones",
+                table: "informacion_auxiliar_temporal",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "usuario_id",
+                schema: "operaciones",
+                table: "informacion_auxiliar",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "usuario_id",
+                schema: "operaciones",
+                table: "informacion_auxiliar_temporal",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "usuario_id",
+                schema: "operaciones",
+                table: "informacion_auxiliar",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
