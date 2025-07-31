b471bf0e5c44541d745c3d1c6ea19e1916b2ec84
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-07-31 14:56:59 -0500

    refactor: 💡 change ClosingDate column type to timestamp

---
+++ src/Modules/Closing/Infrastructure/Database/Migrations/20250731195430_AlterClosingDateColumnTypeToTimestamp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Closing.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterClosingDateColumnTypeToTimestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fecha_proceso",
+                schema: "cierre",
+                table: "valoracion_portafolio",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "fecha_proceso",
+                schema: "cierre",
+                table: "valoracion_portafolio",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
