92ba02851d460f1d09207489d827bcd4879f15ea
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2025-08-05 10:23:16 -0500

    Cambio de tipos de fecha en CreatePensionRequirementInput

--- src/Modules/Associate/Presentation/GraphQL/Inputs/CreatePensionRequirementInput.cs
+++ src/Modules/Associate/Presentation/GraphQL/Inputs/CreatePensionRequirementInput.cs
@@ -5,6 +5,6 @@ namespace Associate.Presentation.GraphQL.Inputs;
 public sealed record CreatePensionRequirementInput(
     [property: GraphQLName("idTipoIdentificacion")] string DocumentType,
     [property: GraphQLName("identificacion")] string Identification,
-    [property: GraphQLName("fechaInicioReqPen")] DateTime? StartDateReqPen,
-    [property: GraphQLName("fechaFinReqPen")] DateTime? EndDateReqPen
+    [property: GraphQLName("fechaInicioReqPen")] DateOnly? StartDateReqPen,
+    [property: GraphQLName("fechaFinReqPen")] DateOnly? EndDateReqPen
 );
