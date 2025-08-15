39d75e3dcaba978c1d2c05c56be104bdf068d99e
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-08-15 09:42:59 -0500

    refactor: 💡 add migration to remove commission fields

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Products/20250815144124_RemoveCommissionFieldsFromPortfoliosTable.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE productos.portafolios DROP COLUMN porcentaje_comision;
+
+ALTER TABLE productos.portafolios DROP COLUMN tipo_tasa_comision;
+
+INSERT INTO productos."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250815144124_RemoveCommissionFieldsFromPortfoliosTable', '9.0.3');
+
+
