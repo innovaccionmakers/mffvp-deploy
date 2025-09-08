31c1752aa021274a1f4d2fc03473be8f11fed2fd
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2025-09-08 12:37:16 -0500

    Alterar columna en informacion_auxiliar y registrar migración

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Operations/20250905171433_AlterTableAuxiliaryInformation.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE operaciones.informacion_auxiliar 
+                ALTER COLUMN cuenta_recaudo TYPE varchar 
+                USING cuenta_recaudo::varchar;
+            
+
+INSERT INTO operaciones."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250905171433_AlterTableAuxiliaryInformation', '9.0.3');
+
+
