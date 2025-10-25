390fa6b0361a68178b9038a7d303086467dbffa2
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2025-10-24 23:31:36 -0500

    add time helper

--- src/Modules/Accounting/Application/Services/AccountingNotificationService.cs
+++ src/Modules/Accounting/Application/Services/AccountingNotificationService.cs
@@ -1,5 +1,6 @@
 using Accounting.Application.Abstractions;
 using Common.SharedKernel.Application.Abstractions;
+using Common.SharedKernel.Application.Helpers.Time;
 using Common.SharedKernel.Domain.Constants;
 using Common.SharedKernel.Infrastructure.NotificationsCenter;
 using Microsoft.Extensions.Configuration;
@@ -10,19 +11,6 @@ public sealed class AccountingNotificationService(
     INotificationCenter notificationCenter,
     IConfiguration configuration) : IAccountingNotificationService
 {
-    private static string FormatDuration(double totalSeconds)
-    {
-        var duration = TimeSpan.FromSeconds(totalSeconds);
-
-        if (duration.TotalDays >= 1)
-            return $"{(int)duration.TotalDays} días";
-        else if (duration.TotalHours >= 1)
-            return $"{(int)duration.TotalHours} horas";
-        else if (duration.TotalMinutes >= 1)
-            return $"{(int)duration.TotalMinutes} minutos";
-        else
-            return $"{(int)totalSeconds} segundos";
-    }
     public async Task SendNotificationAsync(
         string user,
         string status,
@@ -76,13 +64,12 @@ public sealed class AccountingNotificationService(
         DateTime processDate,
         CancellationToken cancellationToken = default)
     {
-        var duration = (DateTime.UtcNow - startDate).TotalSeconds;
         var message = $"Proceso contable {processId} completado exitosamente para la fecha {processDate:yyyy-MM-dd}";
 
         var details = new Dictionary<string, string>
         {
             { "Exitoso", message },
-            { "Duracion", FormatDuration(duration) },
+            { "Duración", TimeHelper.GetDuration(startDate, DateTime.UtcNow) },
             { "Fecha Generacion", processDate.ToString("yyyy-MM-dd") }
         };
 
@@ -108,7 +95,7 @@ public sealed class AccountingNotificationService(
         var details = new Dictionary<string, string>
         {
             { "Error", errorMessage },
-            { "Duracion", FormatDuration(duration) },
+            { "Duración", TimeHelper.GetDuration(startDate, DateTime.UtcNow) },
             { "Fecha Generacion", processDate.ToString("yyyy-MM-dd") },
         };
 
@@ -135,7 +122,7 @@ public sealed class AccountingNotificationService(
         var details = new Dictionary<string, string>
         {
             { "url", reportUrl },
-            { "Duracion", FormatDuration(duration) },
+            { "Duración", TimeHelper.GetDuration(startDate, DateTime.UtcNow) },
             { "Fecha Generacion", processDate.ToString("yyyy-MM-dd") },
             { "Registros Totales", totalRecords.ToString() }
         };
@@ -163,7 +150,7 @@ public sealed class AccountingNotificationService(
 
         var details = new
         {
-            Duracion = FormatDuration(duration),
+            Duracion = TimeHelper.GetDuration(startDate, DateTime.UtcNow),
             FechaGeneracion = processDate.ToString("yyyy-MM-dd"),
             RegistrosTotales = totalRecords,
             Errores = errors
