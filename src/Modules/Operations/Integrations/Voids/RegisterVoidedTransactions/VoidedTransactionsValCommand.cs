02aefca1a99ed4d2c1954ae6ef93590f9df4abd3
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-11-04 08:56:53 -0500

    feat: 🎸 add logs in VoidedTransactionsValCommand

--- src/Modules/Operations/Integrations/Voids/RegisterVoidedTransactions/VoidedTransactionsValCommand.cs
+++ src/Modules/Operations/Integrations/Voids/RegisterVoidedTransactions/VoidedTransactionsValCommand.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using Common.SharedKernel.Application.Attributes;
 using Common.SharedKernel.Application.Messaging;
 
 namespace Operations.Integrations.Voids.RegisterVoidedTransactions;
 
+[AuditLog]
 public sealed record VoidedTransactionsValCommand(
     IReadOnlyCollection<VoidedTransactionItem> Items,
     long CauseId,
