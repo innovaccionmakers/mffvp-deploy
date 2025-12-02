d4c7c31061c8107fddb674353146df27bc78c82e
Santiago Roldan Silva <innovaccion.makersdev2@outlook.com>
2025-12-02 15:52:12 -0500

    feat: 🎸 add auditlog to service consecutive

--- src/Modules/Accounting/Integrations/ConsecutivesSetup/UpdateConsecutiveSetupCommand.cs
+++ src/Modules/Accounting/Integrations/ConsecutivesSetup/UpdateConsecutiveSetupCommand.cs
@@ -1,7 +1,9 @@
+using Common.SharedKernel.Application.Attributes;
 using Common.SharedKernel.Application.Messaging;
 
 namespace Accounting.Integrations.ConsecutivesSetup;
 
+[AuditLog]
 public sealed record UpdateConsecutiveSetupCommand(
     long Id,
     string Nature,
