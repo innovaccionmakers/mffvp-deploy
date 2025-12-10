c1c7998fa4242015ebc7223ac69a7449b907d396
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2025-12-10 15:50:24 -0500

    Implement migration to change account length and identifier type in the accounting database schema

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Accounting/20251210203001_ChangeAccountLength.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE contabilidad.auxiliar_contable ALTER COLUMN identificador TYPE uuid;
+
+ALTER TABLE contabilidad.auxiliar_contable ALTER COLUMN cuenta TYPE character varying(20);
+
+INSERT INTO contabilidad."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251210203001_ChangeAccountLength', '9.0.3');
+
+
---
+++ src/Modules/Accounting/Infrastructure/Database/Migrations/20251210203001_ChangeAccountLength.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAccountLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "identificador",
+                schema: "contabilidad",
+                table: "auxiliar_contable",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "UUID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "cuenta",
+                schema: "contabilidad",
+                table: "auxiliar_contable",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(6)",
+                oldMaxLength: 6,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "identificador",
+                schema: "contabilidad",
+                table: "auxiliar_contable",
+                type: "UUID",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "cuenta",
+                schema: "contabilidad",
+                table: "auxiliar_contable",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
