ee8dab7ee5430ed03c5314fdab0a16c8c652df7e
Katty Mizhquero <innovaccion.makersdev8@outlook.com>
2025-12-22 09:19:05 -0500

    Format 312 - 313 - 314 Business Logic Fix

--- src/Modules/Reports/Application/Reports/TransmissionFormat/TransmissionFormatReport.cs
+++ src/Modules/Reports/Application/Reports/TransmissionFormat/TransmissionFormatReport.cs
@@ -1,4 +1,5 @@
 using Common.SharedKernel.Application.Reports.Strategies;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Reports.Domain.TransmissionFormat;
@@ -123,15 +124,19 @@ public class TransmissionFormatReport(
         var contributionUnitsAdjustment = 0m;
         var withdrawalUnitsAdjustment = 0m;
 
-        if (distributionAmount > 0m)
+        //Conceptos Automáticos: 
+ 
+        //Cuando el concepto < 0 es aporte con signo positivo.
+        //Cuando el concepto es > 0 es retiro con signo negativo.
+        if (distributionAmount < 0m)
         {
-            contributionAmount += distributionAmount;
+            contributionAmount += Math.Abs(distributionAmount);
             if (canAdjustUnits)
             {
-                contributionUnitsAdjustment += distributionAmount / unitValue;
+                contributionUnitsAdjustment += Math.Abs(distributionAmount) / unitValue;
             }
         }
-        else if (distributionAmount < 0m)
+        else if (distributionAmount > 0m)
         {
             withdrawalAmount += distributionAmount;
             if (canAdjustUnits)
@@ -140,16 +145,16 @@ public class TransmissionFormatReport(
             }
         }
 
-        if (notePositiveAmount > 0m)
+        if (notePositiveAmount < 0m)
         {
-            contributionAmount += notePositiveAmount;
+            contributionAmount += Math.Abs(notePositiveAmount);
             if (canAdjustUnits)
             {
-                contributionUnitsAdjustment += notePositiveAmount / unitValue;
+                contributionUnitsAdjustment += Math.Abs(notePositiveAmount) / unitValue;
             }
         }
 
-        if (noteNegativeAmount < 0m)
+        if (noteNegativeAmount > 0m)
         {
             withdrawalAmount += noteNegativeAmount;
             if (canAdjustUnits)
