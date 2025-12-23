32771d113e0ee62c884e68e848ea4e67df6257d3
Johan Sebastian Ramirez Triana <sebastian.ramirez@somosmakers.co>
2025-12-23 11:30:46 -0500

    Create table MinimumWage

---
+++ src/Modules/Products/Domain/MinimumWages/MinimumWage.cs
@@ -0,0 +1,31 @@
+﻿using Common.SharedKernel.Domain;
+
+namespace Products.Domain.MinimumWages
+{
+    public sealed class MinimumWage : Entity
+    {
+        public int MinimumWageId { get; private set; }
+        public string Year { get; private set; }
+        public decimal Value { get; private set; }
+
+        private MinimumWage()
+        {            
+        }
+
+        public static Result<MinimumWage> Create(string year, decimal value)
+        {
+            var minimumWage = new MinimumWage
+            {
+                Year = year,
+                Value = value
+            };
+            return Result.Success(minimumWage);
+        }
+
+        public void UpdateDetails(string newYear, decimal newValue)
+        {
+            Year = newYear;
+            Value = newValue;
+        }
+    }
+}
