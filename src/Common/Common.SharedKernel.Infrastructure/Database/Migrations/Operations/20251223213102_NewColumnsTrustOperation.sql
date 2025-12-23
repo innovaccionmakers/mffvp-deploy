e4507ded28e3f312fde05ce02192b6f3af2027fd
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2025-12-23 16:40:51 -0500

    Add new columns and constraints to TrustOperation and related tables

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Operations/20251223213102_NewColumnsTrustOperation.sql
@@ -0,0 +1,18 @@
+﻿ALTER TABLE operaciones.operaciones_fideicomiso ADD capital_pagado numeric NOT NULL DEFAULT 0.0;
+
+ALTER TABLE operaciones.operaciones_fideicomiso ADD rendimientos_pagados numeric NOT NULL DEFAULT 0.0;
+
+ALTER TABLE operaciones.operaciones_fideicomiso ADD retencion_contingente_retiro numeric NOT NULL DEFAULT 0.0;
+
+ALTER TABLE operaciones.operaciones_fideicomiso ADD retencion_rendimientos_retiro numeric NOT NULL DEFAULT 0.0;
+
+ALTER TABLE operaciones.operaciones_fideicomiso ADD valor_solicitado numeric NOT NULL DEFAULT 0.0;
+
+CREATE INDEX "IX_operaciones_clientes_operaciones_cliente_id" ON operaciones.operaciones_clientes (operaciones_cliente_id);
+
+ALTER TABLE operaciones.operaciones_clientes ADD CONSTRAINT "FK_operaciones_clientes_operaciones_clientes_operaciones_clien~" FOREIGN KEY (operaciones_cliente_id) REFERENCES operaciones.operaciones_clientes (id) ON DELETE RESTRICT;
+
+INSERT INTO operaciones."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251223213102_NewColumnsTrustOperation', '9.0.3');
+
+
--- src/Modules/Operations/Infrastructure/Database/Migrations/20251223213102_NewColumnsTrustOperation.Designer.cs
+++ src/Modules/Operations/Infrastructure/Database/Migrations/20251223213102_NewColumnsTrustOperation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Operations.Infrastructure.Database;
@@ -12,9 +13,11 @@ using Operations.Infrastructure.Database;
 namespace Operations.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(OperationsDbContext))]
-    partial class OperationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251223213102_NewColumnsTrustOperation")]
+    partial class NewColumnsTrustOperation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
