275dfa2dccfe15c38b80b56fcc8dfcf7d447c5f7
innovaccionmakersdev5 <innovaccion.makersdev5@outlook.com>
2026-01-07 09:39:22 -0500

    Update WithdrawalContingentTax precision in database schema for consistency across TrustOperation and OperationsDbContext.

---
+++ src/Common/Common.SharedKernel.Infrastructure/Database/Migrations/Operations/20260107143428_FixConfigureColumnRetencionRendimientoRetiroAddRangeValue.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE operaciones.operaciones_fideicomiso ALTER COLUMN retencion_contingente_retiro TYPE numeric(19,2);
+
+INSERT INTO operaciones."__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20260107143428_FixConfigureColumnRetencionRendimientoRetiroAddRangeValue', '9.0.3');
+
+
---
+++ src/Modules/Operations/Infrastructure/Database/Migrations/20260107143428_FixConfigureColumnRetencionRendimientoRetiroAddRangeValue.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Operations.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixConfigureColumnRetencionRendimientoRetiroAddRangeValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "retencion_contingente_retiro",
+                schema: "operaciones",
+                table: "operaciones_fideicomiso",
+                type: "numeric(19,2)",
+                precision: 19,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldDefaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "retencion_contingente_retiro",
+                schema: "operaciones",
+                table: "operaciones_fideicomiso",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(19,2)",
+                oldPrecision: 19,
+                oldScale: 2,
+                oldDefaultValue: 0m);
+        }
+    }
+}
